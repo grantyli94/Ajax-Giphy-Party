94fee8b2ca22ba73f84a7c45451923484fcf9457
Mike Chang <mchang95@gmail.com>
2021-06-29 16:32:30 -0700

    adds remove button

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,24 +1,28 @@
 console.log("Let's get this party started!");
 
 let submitButton = $("#submit-button");
-submitButton.on("click", function (evt) {
+submitButton.on("click", async function (evt) {
   evt.preventDefault();
-  getGiphy();
-  // createAndAppendImage(url);
+  let url = await getGiphy();
+  createAndAppendImage(url);
 });
 
 async function getGiphy() {
   let textInput = $("#search").val();
   let response = await axios.get('http://api.giphy.com/v1/gifs/search',{params: { q: textInput, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"} })
-  let url = response.data.data[0].images.downsized.url;
+  return response.data.data[0].images.downsized.url;
+}
+
+function createAndAppendImage(url) {
   let newGif = $(`<img src=${url}>`);
   let container = $(".container");
   container.append(newGif);
 }
 
-// function createAndAppendImage(url) {
-//   let newGif = $(`<img src=${url}>`);
-//   let container = $(".container");
-//   container.append(newGif);
-// }
+let removeButton = $("#remove-button");
+removeButton.on("click", function(evt){
+  evt.preventDefault();
+  let container = $(".container");
+  container.empty();
+})
 
